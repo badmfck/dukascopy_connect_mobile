eeb8fec1d58a3b4ed8c6453d75b198154f8bd528
Igors Blums <igors.blums@dukascopy.com>
2021-05-13 13:19:12 +0300

    CONNECT-19027

---
+++ _ios_postproduction/xcode/Dukascopy/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Dukascopy
+//
+//  Created by Mykola Blyk on 12/12/17.
+//  Copyright © 2017 Dukascopy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
